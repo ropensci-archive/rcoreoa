1f83af44818066011ad7d1bef23be4aa13ff71fe
Scott Chamberlain <myrmecocystus@gmail.com>
2017-11-24 09:23:34 -0800

    fix to core_articles_pdf to now check if id has a pdf assoc stop if not found bump version

--- R/core_articles_pdf.R
+++ R/core_articles_pdf.R
@@ -5,9 +5,14 @@
 #' @param id (integer) CORE ID of the article that needs to be fetched.
 #' Required
 #' @param overwrite (logical) overwrite file or not. Default: `TRUE`
-#' @details `core_articles_history` does the HTTP request and parses, while
-#' `core_articles_history_` just does the HTTP request, gives back JSON as
-#' a character string
+#' @details `core_articles_pdf` does the HTTP request and parses
+#' PDF to text, while `core_articles_pdf_` just does the HTTP request
+#' and gives back the path to the file
+#'
+#' We now do a request to [core_articles()] first to find out if there is a
+#' PDF available via existence of the field `fulltextIdentifier`. If not,
+#' we stop with error message; if the field exists, we proceed to
+#' attempting to fetch the PDF.
 #'
 #' These functions take one article ID at a time. Use lapply/loops/etc for
 #' many ids
@@ -31,6 +36,13 @@ core_articles_pdf <- function(id, key = NULL, overwrite = TRUE, parse = TRUE,
 #' @export
 #' @rdname core_articles_pdf
 core_articles_pdf_ <- function(id, key = NULL, overwrite = TRUE, ...) {
-  core_GET_disk(path = sprintf("articles/get/%s/download/pdf", id), id,
+	# check if the ID has a PDF associated
+	x <- core_articles(id, key = key)
+	if (is.null(x$data$fulltextIdentifier)) {
+		stop("no PDF available for ", id, call. = FALSE)
+	} else {
+		# get PDF
+  	core_GET_disk(path = sprintf("articles/get/%s/download/pdf", id), id,
                 key, overwrite, ...)
+	}
 }
